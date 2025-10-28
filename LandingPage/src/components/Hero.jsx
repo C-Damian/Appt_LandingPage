974faaab304f389882637039afc60d9653dc7bdb
C-Damian <138392709+C-Damian@users.noreply.github.com>
2025-10-27 23:13:09 -0500

    Completed hero section

--- LandingPage/src/components/Hero.jsx
+++ LandingPage/src/components/Hero.jsx
@@ -1,36 +1,42 @@
 import { MessageCircle, X } from "lucide-react";
 
+const Items = [
+  { name: 'Portfolio', href: '#portfolio' },
+  { name: 'Catalogo', href: '#catalogo' },
+  { name: 'Certificacion', href: '#certificacion' },
+  { name: 'Consulta', href: '#consulta' }
+]
 
 export default function Hero() {
+  
   return (
     <section id="hero" className="hero-bg">
       <div className="container-max py-12 flex flex-col md:flex-row items-center gap-8">
         <div className="md:w-1/2 flex justify-center">
           <div className="w-full max-w-sm card-soft p-4">
-            <img src="/src/assets/Hero.jpg" alt="artist" className="mt-4 w-full rounded-2xl shadow-lg" />
+            <img src="/src/assets/Hero.jpg" alt="artist" className="mt-4 w-full rounded-2xl shadow-2xl/50" />
           </div>
         </div>
 
         <div className="md:w-1/2">
-          <h2 className="text-3xl font-bold text-blush-900">Marta González — Makeup Artist</h2>
-          <p className="my-1 text-blush-700">Expert in bridal....</p>
+          <h1 className="text-5xl font-bold text-blush-900 text-center px-3 font-family header-text">Silvia Trujillo</h1>
+          <h5 className="text-blush-700 text-center mb-4 font-bold">Professional Makeup Artist</h5>
 
           <div className="mt-6 flex items-center gap-4 justify-center">
-            <button className="flex items-center gap-2 btn-primary"> 
-              <MessageCircle className="w-4 h-4" />
-              WhatsApp
+            <button className="flex items-center gap-2">
+              <img className="w-12 h-12" src="/src/assets/whatsapp-circle.svg"></img>
             </button>
-            <button className="flex items-center gap-2 btn-ghost">
-              <svg className="w-4 h-4" viewBox="0 0 24 24" fill="currentColor"><path d="M12 2C8.13 2 5 5.13 5 9c0 4.87 7 13 7 13s7-8.13 7-13c0-3.87-3.13-7-7-7z"/></svg>
-              TikTok
+            <button className="flex items-center gap-2">
+              <img className="w-12 h-12" src="/src/assets/tiktok-circle.svg"></img>
             </button>
           </div>
 
-          <div className="mt-6 grid grid-cols-2 gap-3">
-            <a href="#portfolio" className="block text-center py-2 btn-ghost">Portfolio</a>
-            <a href="#consulta" className="block text-center py-2 btn-primary">Consulta</a>
-            <a href="#catalogo" className="block text-center py-2 btn-ghost">Catalogo de servicios</a>
-            <a href="#certificacion" className="block text-center py-2 btn-ghost">Certificacion</a>
+          <div className="mt-3 grid grid-cols-2 p-4 gap-3">
+            {Items.map((item, key) => (
+              <a key={item.key} href={item.href} className="new-button block text-center py-2 inset-ring-2 inset-ring-rose-200">
+                {item.name}
+              </a>
+            ))}
           </div>
         </div>
       </div>
