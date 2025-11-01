ae50c6297720383c06f6d5e6b93dcd0de41a9824
C-Damian <138392709+C-Damian@users.noreply.github.com>
2025-10-31 21:08:23 -0500

    fixed mobile compt

--- LandingPage/src/components/StickyConsultButton.jsx
+++ LandingPage/src/components/StickyConsultButton.jsx
@@ -2,9 +2,9 @@ import { MessageSquareHeart } from "lucide-react"
 
 export default function StickyConsultButton() {
   return (
-    <a href="#consulta" className="fixed right-4 bottom-2 z-50 animate-bounce">
-      <button className="bg-foreground new-button from-blush-400 to-blush-500 h-12 font-bold pulse">
-       <MessageSquareHeart className="w-8 h-8 text-white" />
+    <a href="#consulta" className="fixed right-4 bottom-154 z-50 animate-bounce">
+      <button className="bg-foreground new-button from-blush-400 to-blush-500 h-8 font-bold pulse">
+       <MessageSquareHeart className="w-5 h-5 text-white" />
       </button>
     </a>
   )
